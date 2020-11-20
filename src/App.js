153ee060168a18b35ddc83ae1fc13a2b1072a9a0
Levon_Yeghiazaryan <levon.y@ucraft.com>
2020-11-20 17:27:19 +0400

    grpc react example initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+import {PageUrl} from "./page_information_pb"
+import {PageInformationServicePromiseClient} from "./page_information_grpc_web_pb"
+
+var client = new PageInformationServicePromiseClient('http://localhost:8000')
+
+function App() {
+    const [temp, setTemp] = useState(-9999);
+
+    const getTemp = () => {
+        console.log("called")
+
+        var sensorRequest = new PageUrl()
+        sensorRequest.setPageUrl("https://github.com/grpc/grpc-web/releases")
+        var stream = client.getPageScreenshot(sensorRequest)
+        stream.then((data)=> {
+            console.log(data)
+        })
+        // stream.on('data', function (response) {
+        //     console.log(1)
+        //     // setTemp(response.getValue())
+        // });
+    };
+
+
+
+    useEffect(() => {
+        getTemp()
+    }, []);
+
+    return (
+        <div>
+            Temperature : {temp} F
+        </div>
+    );
+}
+
+export default App;
---
+++ src/page_information_grpc_web_pb.js
@@ -0,0 +1,154 @@
+/**
+ * @fileoverview gRPC-Web generated client stub for page_information
+ * @enhanceable
+ * @public
+ */
+/* eslint-disable */
+// GENERATED CODE -- DO NOT EDIT!
+
+
+/* eslint-disable */
+// @ts-nocheck
+
+
+
+const grpc = {};
+grpc.web = require('grpc-web');
+
+const proto = {};
+proto.page_information = require('./page_information_pb.js');
+
+/**
+ * @param {string} hostname
+ * @param {?Object} credentials
+ * @param {?Object} options
+ * @constructor
+ * @struct
+ * @final
+ */
+proto.page_information.PageInformationServiceClient =
+    function(hostname, credentials, options) {
+  if (!options) options = {};
+  options['format'] = 'text';
+
+  /**
+   * @private @const {!grpc.web.GrpcWebClientBase} The client
+   */
+  this.client_ = new grpc.web.GrpcWebClientBase(options);
+
+  /**
+   * @private @const {string} The hostname
+   */
+  this.hostname_ = hostname;
+
+};
+
+
+/**
+ * @param {string} hostname
+ * @param {?Object} credentials
+ * @param {?Object} options
+ * @constructor
+ * @struct
+ * @final
+ */
+proto.page_information.PageInformationServicePromiseClient =
+    function(hostname, credentials, options) {
+  if (!options) options = {};
+  options['format'] = 'text';
+
+  /**
+   * @private @const {!grpc.web.GrpcWebClientBase} The client
+   */
+  this.client_ = new grpc.web.GrpcWebClientBase(options);
+
+  /**
+   * @private @const {string} The hostname
+   */
+  this.hostname_ = hostname;
+
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.page_information.PageUrl,
+ *   !proto.page_information.PageScreenshot>}
+ */
+const methodDescriptor_PageInformationService_GetPageScreenshot = new grpc.web.MethodDescriptor(
+  '/page_information.PageInformationService/GetPageScreenshot',
+  grpc.web.MethodType.UNARY,
+  proto.page_information.PageUrl,
+  proto.page_information.PageScreenshot,
+  /**
+   * @param {!proto.page_information.PageUrl} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.page_information.PageScreenshot.deserializeBinary
+);
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.page_information.PageUrl,
+ *   !proto.page_information.PageScreenshot>}
+ */
+const methodInfo_PageInformationService_GetPageScreenshot = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.page_information.PageScreenshot,
+  /**
+   * @param {!proto.page_information.PageUrl} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.page_information.PageScreenshot.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.page_information.PageUrl} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.page_information.PageScreenshot)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.page_information.PageScreenshot>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.page_information.PageInformationServiceClient.prototype.getPageScreenshot =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/page_information.PageInformationService/GetPageScreenshot',
+      request,
+      metadata || {},
+      methodDescriptor_PageInformationService_GetPageScreenshot,
+      callback);
+};
+
+
+/**
+ * @param {!proto.page_information.PageUrl} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.page_information.PageScreenshot>}
+ *     Promise that resolves to the response
+ */
+proto.page_information.PageInformationServicePromiseClient.prototype.getPageScreenshot =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/page_information.PageInformationService/GetPageScreenshot',
+      request,
+      metadata || {},
+      methodDescriptor_PageInformationService_GetPageScreenshot);
+};
+
+
+module.exports = proto.page_information;
+
